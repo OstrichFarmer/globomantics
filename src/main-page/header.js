bfdb4f8a17d1fe4c08197860ca4279757ae7506a
Joel Stephen <joel.stephen56.sj@gmail.com>
2023-08-08 16:15:57 +0100

    added props to my project

--- src/main-page/header.js
+++ src/main-page/header.js
@@ -1,9 +1,9 @@
 import logo from "./GlobomanticsLogo.png";
 
-const Header = () => (
+const Header = ({subtitle}) => (
     <header className="row">
         <div className="col-md-5"><img src={logo} className="logo" alt="logo" /></div>
-        <div className="col-md-7 mt-5 subtitle">Providing houses all over the world</div>
+        <div className="col-md-7 mt-5 subtitle">{subtitle} </div>
     </header>
 );
 export default Header;
