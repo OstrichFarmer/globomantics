6c5a49a696fcd4616c224c60613ab67389a51513
Joel Stephen <joel.stephen56.sj@gmail.com>
2023-08-08 15:41:37 +0100

    ran the project

--- src/main-page/header.js
+++ src/main-page/header.js
@@ -3,6 +3,7 @@ import logo from "./GlobomanticsLogo.png";
 const Header = () => (
     <header className="row">
         <div className="col-md-5"><img src={logo} className="logo" alt="logo" /></div>
-        <div className="col-md-7 mt-5 subtitle"></div>
+        <div className="col-md-7 mt-5 subtitle">Providing houses all over the world</div>
     </header>
 );
+export default Header;
