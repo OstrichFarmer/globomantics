cac25cd1c2882a13ded6fa4d1f242c66991a08c6
Joel Stephen <joel.stephen56.sj@gmail.com>
2023-08-08 17:25:57 +0100

    implemented useEffect

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,8 +1,14 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './main-page.css';
 import Header from './header';
 
 function App() {
+useEffect(()=>{
+  const fetchHouse = async () => {
+    const rsp =await fetch("./houses.json");
+    const houses = await rsp.json;
+  };fetchHouse();
+})
   return (
     <div className="Container">
       <Header subtitle="Providing houses all over the world"/>
