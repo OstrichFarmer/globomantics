b237984895e6cb08c33c520778b7bcf3f3f39310
Joel Stephen <joel.stephen56.sj@gmail.com>
2023-08-09 15:05:20 +0100

    used useMemo hook from react

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,14 +1,24 @@
-import { useEffect } from 'react';
+import {useState, useEffect, useMemo} from 'react';
 import './main-page.css';
 import Header from './header';
 
 function App() {
+  const [allHouses, setAllHouses] = useState([]);
 useEffect(()=>{
   const fetchHouse = async () => {
     const rsp =await fetch("./houses.json");
     const houses = await rsp.json;
+    setAllHouses();
   };fetchHouse();
-})
+},[])
+
+ const featuredHouse = useMemo(()=>{
+  if(allHouses.length){
+    const randomIndex = Math.floor(Math.random() * allHouses.length);
+    return allHouses[randomIndex]
+  }
+}, [allHouses])
+
   return (
     <div className="Container">
       <Header subtitle="Providing houses all over the world"/>
