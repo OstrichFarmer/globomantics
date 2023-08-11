436471128fdd70c130232e377989df504cb0f175
Joel Stephen <joel.stephen56.sj@gmail.com>
2023-08-11 18:28:07 +0100

    fixes

--- src/main-page/header.js
+++ src/main-page/header.js
@@ -1,9 +1,11 @@
 import logo from "./GlobomanticsLogo.png";
 
-const Header = ({subtitle}) => (
-    <header className="row">
-        <div className="col-md-5"><img src={logo} className="logo" alt="logo" /></div>
-        <div className="col-md-7 mt-5 subtitle">{subtitle} </div>
-    </header>
+const Header = ({ subtitle }) => (
+  <header className="row">
+    <div className="col-md-5">
+      <img src={logo} className="logo" alt="logo" />
+    </div>
+    <div className="col-md-7 mt-5 subtitle">{subtitle} </div>
+  </header>
 );
 export default Header;
--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,36 +1,39 @@
-import {useState, useEffect, useMemo} from 'react';
-import './main-page.css';
-import Header from './header';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useState, useEffect, useMemo } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./main-page.css";
+import Header from "./header";
+import FeaturedHouse from "./featured-house";
 
 function App() {
   const [allHouses, setAllHouses] = useState([]);
-useEffect(()=>{
-  const fetchHouse = async () => {
-    const rsp =await fetch("./houses.json");
-    const houses = await rsp.json;
-    setAllHouses();
-  };fetchHouse();
-},[])
 
- const featuredHouse = useMemo(()=>{
-  if(allHouses.length){
-    const randomIndex = Math.floor(Math.random() * allHouses.length);
-    return allHouses[randomIndex]
-  }
-}, [allHouses])
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const rsp = await fetch("./houses.json");
+      const houses = await rsp.json;
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, []);
+
+  const featuredHouse = useMemo(() => {
+    if (allHouses.length) {
+      const randomIndex = Math.floor(Math.random() * allHouses.length);
+      return allHouses[randomIndex];
+    }
+  }, [allHouses]);
 
   return (
     <Router>
-      <div className="Container">
-      <Header subtitle="Providing houses all over the world"/>
+      <div className="container">
+        <Header subtitle="Providing houses all over the world" />
 
-      <Switch>
-        <Route path='/'>
-
-        </Route>
-      </Switch>
-    </div>
+        <Switch>
+          <Route path="/">
+            <FeaturedHouse house={featuredHouse}/>
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
